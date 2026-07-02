d9455edd2164859bc3de59e8eca2c3e3c5484e2a
Nikolas <nikolasdtesch@gmail.com>
2026-07-02 19:36:00 -0300

    perf(web): preconnect material icon font origins

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -31,6 +31,8 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" className={`dark ${ibmPlexSans.variable} ${ibmPlexMono.variable}`}>
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
